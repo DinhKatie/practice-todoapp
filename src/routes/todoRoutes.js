9469a6dfb2c465c0b7aaa4d8ad3949780a865e2c
DinhKatie <ikatiedinh@gmail.com>
2026-07-01 11:55:08 -0700

    finish todo routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -13,17 +13,31 @@ router.get('/', (req, res) => {
 
 // Create todo
 router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
 
+    res.json({id: result.lastInsertRowid, task, completed: 0})
 })
 
 // Update todo
 router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const {id } = req.params
+    // const { page } = req.query EXAMPLE
+    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    updatedTodo.run(completed, id)
+    res.json({message: "Todo completed"})
 
 })
 
 // Delete todo
 router.delete('/:id', (req, res) => {
-
+    const {id} = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId)
+    res.send({message: "Todo deleted"})
 })
 
 export default router
